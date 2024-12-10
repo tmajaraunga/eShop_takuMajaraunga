e067997ff97308322f5955ce0bb1b644e6c9f27a
tmajaraunga <tmajaraunga@css.edu>
2024-12-10 11:52:58 -0600

    Updated the Catalog Database to include the new item(gender)

---
+++ src/Catalog.API/Infrastructure/Migrations/20241210174239_PendingChanges.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Catalog.API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "gender",
+                table: "Catalog",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "gender",
+                table: "Catalog");
+        }
+    }
+}
